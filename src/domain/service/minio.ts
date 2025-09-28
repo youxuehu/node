c9fb056cfb18b5e2afe5e8c7308dbf950efae494
youxuehu <youxuehu@hotmail.com>
2025-09-29 00:19:47 +0800

    添加minio文件上传接口

---
+++ src/domain/service/minio.ts
@@ -0,0 +1,39 @@
+import { Logger } from 'winston'
+import { SingletonLogger } from '../facade/logger'
+import * as Minio from 'minio';
+
+export class MinioService {
+    private logger: Logger = SingletonLogger.get()
+
+    async getClient() {
+        const endPoint = process.env.MINIO_ENDPOINT || '127.0.0.1'
+        const accessKey = process.env.MINIO_ACCESS_KEY || 'H9zLWI2iXRGIeO46QmLW'
+        const secretKey = process.env.MINIO_SECRET_KEY || 'gQyqHBJpyWI0CJ3jdOi8K3vI41JLctuFPFCCuWtE'
+        
+        const minioClient: Minio.Client = new Minio.Client({
+            endPoint: endPoint, 
+            port: 9000,
+            useSSL: true,
+            accessKey: accessKey,
+            secretKey: secretKey,
+        });
+        return minioClient
+    }
+
+    async getUrl(filename: string): Promise<string> {
+        const bucketName = process.env.MINIO_BUCKET || 'public-bucket';
+        
+        // ✅ 先 await 得到 client 实例
+        const client = await this.getClient();
+        
+        // ✅ 再调用 presignedPutObject
+        const url = await client.presignedPutObject(
+            bucketName,
+            filename,
+            3600 // 1小时有效
+        );
+
+        return url;
+    }
+    
+}
